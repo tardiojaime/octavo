482e336221a86bddaa62e2e45ecb4acf93c5c41a
tardio717 <tardiojaime@users.noreply.github.com>
2023-10-13 09:54:47 -0400

    version 01

--- script.js
+++ script.js
@@ -2,32 +2,34 @@
 var btn_submit = document.getElementById("btn_submit");
 var form = document.getElementById("registration-form");
 var espera = document.getElementById("espera");
+var listo = document.getElementById("listo");
+
 form.addEventListener("submit", function (event) {
   event.preventDefault();
-  btn_submit.setAttribute("disabled", "disabled");
-  // Aquí puedes agregar código para procesar los datos del formulario, como enviarlos a un servidor o mostrar un mensaje de confirmación.
-  btn_submit.text = "Enviando";
-  espera.classList.remove("d-none");
+  btn_submit.disabled = true;
   const formData = new FormData(this);
-  formData.forEach(function (valor, clave) {
-    console.log(clave + ": " + valor);
-  });
-  /*   const url =
+  const url =
     "https://script.google.com/macros/s/AKfycbwzbV5EH25GK4FISI6LqvFr4a1A0f-DSEgAos6-R4fi-pInPzlxlm9n5jwfPuGHhVHMLw/exec";
   fetch(url, { method: "POST", body: formData })
     .then((response) => response.json())
-    .then((res) => {})
-    .catch((error) => console.log(" Error: " + error.message)); */
+    .then((res) => {
+      guardar();
+    })
+    .catch((error) => console.log(" Error: " + error.message));
 });
+function guardar() {
+  espera.classList.add("d-none");
+  listo.classList.remove("d-none");
+  btn_submit.textContent = "Enviado";
+}
 function ruta(sms) {
-  let numero = 59167638517;
-  //https://api.whatsapp.com/send?phone=NUMERO&text=MENSAJE
+  let numero = 59172871823;
   const ruta = `https://api.whatsapp.com/send?phone=+${numero}&text=${sms}`;
   return ruta;
 }
 document.getElementById("wa-me").addEventListener("click", function (e) {
   e.preventDefault();
-  let sms = "Hola deseo reservar un curso";
+  let sms = "Hola, deseo reservar un curso:";
   window.open(ruta(sms), "_blank");
 });
 var cursos = [
